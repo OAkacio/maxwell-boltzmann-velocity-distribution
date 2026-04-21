e78cc5287548170f99cbbe0187b4ad6809a03f79
Victor Moreira Acacio <victor.mor.acacio@gmail.com>
2026-04-21 16:56:01 -0300

    Formatação: Formatação básica

--- src/utils.py
+++ src/utils.py
@@ -4,7 +4,9 @@ from astropy import constants as cons
 import numpy as np
 
 #! -------------------------------------------------- Constantes Físicas --------------------------------------------------
-R = cons.R.value * 1000 # Multiplicado por 1000 para passar a massa molar utilizada ser em gramas por mol.
+R = (
+    cons.R.value * 1000
+)  # Multiplicado por 1000 para passar a massa molar utilizada ser em gramas por mol.
 zero_abs = -273.15
 pi = np.pi
 e = np.e
